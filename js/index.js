9dbb37c69b524567d7e7529a7f03c06d27b329d2
Du2Du <eduardohilario.eha@gmail.com>
2022-12-05 21:08:41 -0300

    Iniciando listagem dos projetos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 function redirectPage(url) {
   window.location.assign(url);
 }
+
+const projects = [
+  {
+    projectName: "teste",
+  },
+];
+
+window.addEventListener("load", () => {
+  const projectsDiv = document.getElementById("projects");
+  projectsDiv.innerHTML = projects.map((project) => project.projectName);
+});
